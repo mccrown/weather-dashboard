2ddf8695b31b954404bf568f7361917ca2d89ae3
CMcCown <connermccown94@gmail.com>
2022-02-26 15:00:47 -0700

    added get coordinates function for get forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,35 @@ var searchTerms = [];
 // Api Key
 var APIKey = "86ef247d424c25935fd6c766a8b744f8";
 
+var getCoordinates = function(searchTerm) {
+    /* use the mapquest API to geocode the location based on the search terms */
+
+    searchTerm = searchTerm.split(" ").join("+");
+    var geocodingApiUrl = "https://www.mapquestapi.com/geocoding/v1/address?key=ZJUiXdZZzhsEe05eUGvmmAsIoTPvQOHn&location=" + searchTerm;
+    fetch(geocodingApiUrl).then(function(res) {
+        if (res.ok) {
+            res.json().then(function(data) {
+
+                // find one location to use to generate the weather
+                var locations = data.results[0].locations;
+                if (locations.length == 1) {
+                    saveLocation(locations[0]);
+                    getWeather(locations[0].latLng);
+                } else {
+                    confirmLocation(locations);  // prompt the user to confirm the location
+                }
+            })
+        } else {
+            console.log("Couldn't get the coordinates from the mapquest API: ", res.text);
+        }
+    });
+}
+
 // get forecast function
 var getForecast = function(cityName) {
     // weather request from api
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + APIKey;
+    var betterUrl = 
 
     fetch(apiUrl).then(function(response){
         if (response.ok) {
@@ -41,6 +66,7 @@ var getForecast = function(cityName) {
     });
 };
 
+
 var displayForecast = function(weatherData) {
 
     // set city name and date
@@ -51,8 +77,16 @@ var displayForecast = function(weatherData) {
     // insert city name and date to page
     nameEl.textContent = city + " (" + formattedDate + ") :";
 
-    // display current forecast
-    var 
+    // display current forecast icon
+    var weatherPic = weatherData.weather[0].icon;
+    currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+    currentPicEl.setAttribute("alt", weatherData.weather[0].description);
+
+    // display temp, humidity, wind speed, uv index
+    currentTempEl.innerHTML = "Temperature: " + weatherData.main.temp + "<span>&#176</span>" + "F";
+    currentHumidityEl.innerHTML = "Humidity: " + weatherData.main.humidity + " %";
+    currentWindEl.innerHTML = "Wind: " + weatherData.wind.speed + "MPH";
+    currentUVEl.innerHTML = "UV Index: " + weatherData.uvi;
 
 }
 
